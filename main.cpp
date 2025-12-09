ac3b4e1215c174522a2a5f97d484b9a6b6e67e60
sodino <sodino@qq.com>
2025-12-09 16:41:00 +0800

    [dev] main : requestAdapter success.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,58 @@
+#include "webgpu-utils.h"
+#include <webgpu/webgpu.h>
 
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+
+class Application {
+public:
+    Application();
+
+    ~Application();
+
+
+    bool Initialize();
+    void Terminate();
+    void MainLoop();
+    bool IsRunning();
+private:
+};
 
 int main() {
+    Application app;
+    if (!app.Initialize()) {
+        std::cout << "Failed to initialize application." << std::endl;
+        return 1;
+    }
     return 1;
+}
+
+
+Application::Application() {
+}
+Application::~Application() {
+}
+bool Application::Initialize() {
+    WGPUInstance instance = wgpuCreateInstance(nullptr);
+    std::cout << "-> Created WebGPU instance: " << instance << std::endl;
+
+    WGPURequestAdapterOptions adapterOpts = {};
+    adapterOpts.nextInChain = nullptr;
+    WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
+    if (adapter == nullptr) {
+        std::cout << "-> Failed to get WebGPU adapter." << std::endl;
+        return false;
+    }
+    std::cout << "-> Got WebGPU adapter: " << adapter << std::endl;
+    inspectAdapter(adapter);
+    return true;
+}
+void Application::Terminate() {
+}
+void Application::MainLoop() {
+}
+bool Application::IsRunning() {
+    return false;
 }
--- webgpu-utils.cpp
+++ webgpu-utils.cpp
@@ -35,3 +35,45 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance, WGPURequestAdapterOptions 
     return userData.adapter;
 }
 
+void inspectAdapter(WGPUAdapter adapter) {
+    WGPUSupportedLimits supportedLimits = {};
+    supportedLimits.nextInChain = nullptr;
+    bool success = wgpuAdapterGetLimits(adapter, &supportedLimits);
+    if (success) {
+        std::cout << "Adapter limits:" << std::endl;
+        std::cout << "  maxTextureDimension1D: " << supportedLimits.limits.maxTextureDimension1D << std::endl;
+        std::cout << "  maxTextureDimension2D: " << supportedLimits.limits.maxTextureDimension2D << std::endl;
+        std::cout << "  maxTextureDimension3D: " << supportedLimits.limits.maxTextureDimension3D << std::endl;
+        std::cout << "  maxTextureArrayLayers: " << supportedLimits.limits.maxTextureArrayLayers << std::endl;
+        std::cout << "  maxBindGroups: " << supportedLimits.limits.maxBindGroups << std::endl;
+    }
+
+
+    std::vector<WGPUFeatureName> features;
+    // 先取:特性数量
+    size_t featureCount = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+    features.resize(featureCount);
+    // 再取:特性列表
+    wgpuAdapterEnumerateFeatures(adapter, features.data());
+    std::cout << "Adapter features:" << std::endl;
+    std::cout << std::hex;
+    for (WGPUFeatureName f : features) {
+        std::cout << " - 0x" << f << std::endl;
+    }
+    std::cout << std::dec;
+
+    WGPUAdapterProperties properties = {};
+    wgpuAdapterGetProperties(adapter, &properties);
+    std::cout << "Adapter properties:" << std::endl;
+    std::cout << "  vendorID: " << properties.vendorID << std::endl;
+    std::cout << "  vendorName: " << (properties.vendorName ? properties.vendorName : "N/A") << std::endl;
+    std::cout << "  architecture: " << (properties.architecture ? properties.architecture : "N/A") << std::endl;
+    std::cout << "  deviceID: " << properties.deviceID << std::endl;
+    std::cout << "  name: " << (properties.name ? properties.name : "N/A") << std::endl;
+    std::cout << "  driverDescription: " << (properties.driverDescription ? properties.driverDescription : "N/A") << std::endl;
+    
+    std::cout << std::hex;
+    std::cout << "  adapterType: " << properties.adapterType << std::endl;
+    std::cout << "  backendType: " << properties.backendType << std::endl;
+    std::cout << std::dec;
+}
