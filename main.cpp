4c255ceb048c2ac8f76513cb9b4af9fcbec4d59e
sodino <sodino@qq.com>
2026-01-06 17:27:54 +0800

    [demo] 简化代码 : 恢复回全量更新 MyUniforms 的值。

--- main.cpp
+++ main.cpp
@@ -625,12 +625,7 @@ void Application::MainLoop() {
     my.color = tmp;
     my.x = cosf(t);
     my.y = sinf(t);
-    queue.writeBuffer(bufUniform, offsetof(MyUniforms, color), &my.color, 4 * sizeof(float));
-    // queue.writeBuffer(bufUniform, 0, &my, sizeof(MyUniforms));
-    // 故意分成两次write，虽然也可以，但这违背了uniform的设计思想：uniform 不是“字段级更新”，而是“16 字节块级一致性模型”，存在数据不同步的风险。
-    std::cout << "TestDemo : offset x=" << offsetof(MyUniforms, x) << " y="<< offsetof(MyUniforms, y) << std::endl;
-    queue.writeBuffer(bufUniform, offsetof(MyUniforms, x), &my.x, sizeof(float));
-    queue.writeBuffer(bufUniform, offsetof(MyUniforms, y), &my.y, sizeof(float));
+    queue.writeBuffer(bufUniform, 0, &my, sizeof(MyUniforms));
 
 	// Create a command encoder for the draw call
 	// WGPUCommandEncoderDescriptor encoderDesc = {};
