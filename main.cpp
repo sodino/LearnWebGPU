06fd0ac592808c9658c5d2f790012f62ab9d95dc
sodino <sodino@qq.com>
2026-01-07 18:28:49 +0800

    [matrices] 效果3：在效果2的基础上，金字塔以原点为圆心，以0.5为半径，整体绕圈圈（同时也在自旋）

--- main.cpp
+++ main.cpp
@@ -97,7 +97,13 @@ fn vs_main(in: VertexInput) -> VertexOutput {
     // 金字塔 1.缩小到原来的0.3；
     //       2.XY平面/Z轴，自旋；
     //       3.向X轴正方向平移0.5。
-    let position = (T * R1 * S * homogeneous_position).xyz;
+    // let position = (T * R1 * S * homogeneous_position).xyz;
+
+
+    // 金字塔 1.缩小到原来的0.3；
+    //       2.向X轴正方向平移0.5；
+    //       3.在XY平面/Z轴，自旋 + 整体在XY平面/Z轴，以第1步的0.5为半径绕圈圈。
+    let position = (R1 * T * S * homogeneous_position).xyz;
 
 	out.position = vec4<f32>(position.x, position.y * data.ratio, position.z * 0.5 + 0.5, 1.0);
     out.color = in.color; // 向片段着色器转发 颜色值
