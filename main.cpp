c19d63dad5a0ea45e66c7f716c526f25fa8350d4
sodino <sodino@qq.com>
2026-02-24 13:59:07 +0800

    [dev] 增加注释。

--- main.cpp
+++ main.cpp
@@ -562,8 +562,8 @@ void Application::MainLoop() {
 
     renderPass.setPipeline(pipeline);
     // renderPass.draw(3, 1, 0, 0);
-    renderPass.setVertexBuffer(0, bufPosition, 0, bufPosition.getSize()); // 对应 @location(0)，也对应 VertexBufferLayout 数组中的第一个VertexBufferLayout
-    renderPass.setVertexBuffer(1, bufColor, 0, bufColor.getSize());       // 对应 @location(1)，也对应 VertexBufferLayout 数组中的第二个VertexBufferLayout
+    renderPass.setVertexBuffer(0, bufPosition, 0, bufPosition.getSize()); // 绑定颜色缓冲区到顶点缓冲槽0（对应第一个VertexBufferLayout〈位置数据〉，该layout内的attribute设置了shaderLocation=0，这里的0与顶点缓冲槽0无关）
+    renderPass.setVertexBuffer(1, bufColor, 0, bufColor.getSize());       // 绑定颜色缓冲区到顶点缓冲槽1（对应第二个VertexBufferLayout〈颜色数据〉，该layout内的attribute设置了shaderLocation=1，这里的1与顶点缓冲槽1无关）
     renderPass.draw(vertexCount, 1, 0, 0);  // 1 : 用当前vertexBuffer，绘制一个物体（虽然这一个物体是2个三角形）。当参数大于1时，即同一份vertexBuffer绘制多份，每一份的位置、颜色可在shader中通过instance_id进行变化（当前代码示例还未涉及）
 
 	renderPass.end();
